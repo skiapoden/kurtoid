dbfb7a2e6395425774b0caa9903fa72fb2728a32
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-10-27 18:22:11 +0100

    added parameter to simulation: number of rounds to play

--- app/simulation.py
+++ app/simulation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 from jass.base.const import JASS_SCHIEBER_2500
 from jass.arena.arena import Arena
 from jass.arena.trump_selection_players_strategy import TrumpPlayerStrategy
@@ -8,7 +10,7 @@ from jass.player.random_player_schieber import RandomPlayerSchieber
 
 from kurtoid import Kurtoid
 
-def main():
+def main(n_games):
     arena = Arena(jass_type=JASS_SCHIEBER_2500,
                   trump_strategy=TrumpPlayerStrategy(),
                   play_game_strategy=PlayNrRoundsStrategy(4))
@@ -17,7 +19,7 @@ def main():
     kurtoid = Kurtoid()
     arena.set_players(kurtoid, opponent, kurtoid, opponent)
 
-    arena.nr_games_to_play = 1000
+    arena.nr_games_to_play = n_games
     arena.play_all_games()
 
     kurtoid_wins = arena.nr_wins_team_0 
@@ -25,10 +27,13 @@ def main():
     draws = arena.nr_draws
     total = kurtoid_wins + opponent_wins + draws
 
-    print('Kurtoid Wins:  {:4d}  {:.1f}%'.format(kurtoid_wins, kurtoid_wins / total * 100))
-    print('Opponent Wins: {:4d}  {:.1f}%'.format(opponent_wins, opponent_wins / total * 100))
-    print('Draws:         {:4d}  {:.1f}%'.format(draws, draws / total * 100))
+    print('Kurtoid  {:4d} {:5.1f}%'.format(kurtoid_wins, kurtoid_wins / total * 100))
+    print('Opponent {:4d} {:5.1f}%'.format(opponent_wins, opponent_wins / total * 100))
+    print('Draws    {:4d} {:5.1f}%'.format(draws, draws / total * 100))
 
 
 if __name__ == '__main__':
-    main()
+    n_games = 1000
+    if len(sys.argv) > 1:
+        n_games = int(sys.argv[1])
+    main(n_games)
