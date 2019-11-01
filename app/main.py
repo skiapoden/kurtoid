a258fffa07f3cf166e2f01491c2ef3f5f8a9eec6
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-11-01 16:24:42 +0100

    added all three available players

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from jass.player_service.player_service_app import PlayerServiceApp
 from jass.player.random_player_schieber import RandomPlayerSchieber
 
 from kurtoid import Kurtoid
+from donat_trump import DonatTrump
 
 def create_app():
     app = PlayerServiceApp('kurtoid_service')
     app.add_player('Kurt', Kurtoid())
-    app.add_player('Hans', Kurtoid())
-    app.add_player('Sepp', Kurtoid())
-    app.add_player('Schorsch', Kurtoid())
+    app.add_player('Maurin', DonatTrump())
+    app.add_player('Randy', RandomPlayerSchieber())
     return app
 
 app = create_app()
