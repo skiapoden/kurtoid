adcd4e3c8bbf96e25808c7a9f7a7e481d01cd9cd
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-11-01 20:55:14 +0100

    slight refactoring of play_card method

--- app/kurtoid.py
+++ app/kurtoid.py
@@ -36,17 +36,17 @@ class Kurtoid(Player):
         best_result = np.iinfo(np.int32).min
         best_card = -1
         for card_index in valid_card_indices:
-            current_trick = rnd.current_trick
+            simulated_trick = rnd.current_trick
 
             # put our card
-            current_trick[our_trick_index] = card_index
+            simulated_trick[our_trick_index] = card_index
 
             # simulate others cards
             cards_missing = MAX_PLAYER - our_trick_index
             others_choices = np.random.choice(others_indices, cards_missing, replace=False)
-            np.put(current_trick, range(our_trick_index+1, MAX_PLAYER+1), others_choices)
+            np.put(simulated_trick, range(our_trick_index+1, MAX_PLAYER+1), others_choices)
 
-            round_result = self.calculate_outcome(rnd, current_trick)
+            round_result = self.calculate_outcome(rnd, simulated_trick)
             if round_result > best_result:
                 best_result = round_result
                 best_card = card_index
